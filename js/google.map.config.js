8c411942d1fbf7269058817a01492fe7526dfb73
manvelmar <manuel.velasco.marcelo@gmail.com>
2016-12-20 02:34:23 +0100

    Update google.map.config.js

--- js/google.map.config.js
+++ js/google.map.config.js
@@ -6,7 +6,7 @@
 		if ($("#map-canvas").length>0) {
 			var map, myLatlng, myZoom, marker;
 			// Set the coordinates of your location
-			myLatlng = new google.maps.LatLng(41.38791700, 2.16991870);
+			myLatlng = new google.maps.LatLng(41.055725, -3.989666);
 			myZoom = 12;
 			function initialize() {
 				var mapOptions = {
